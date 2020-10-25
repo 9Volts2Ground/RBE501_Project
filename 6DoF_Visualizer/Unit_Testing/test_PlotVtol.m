af70f14a10d1f43a3b40d1ab024b911b99bc48e8
9Volts2Ground <s.adam.stringham@gmail.com>
2020-10-24 20:23:01 -0700

    Added .gitignore and /Unit_Test directory. Cleaned up repo bloat from homework examples.

---
+++ 6DoF_Visualizer/Unit_Testing/test_PlotVtol.m
@@ -0,0 +1,17 @@
+clc; clear all; close all; 
+
+addpath('..')
+
+vtol_param;
+
+pos = [10,0,10];
+orientation = [pi/6,pi/8,0];
+time = 0;
+
+%Pack data into array
+u(1:3) = pos;
+u(4:6) = orientation;
+u(7) = time;
+
+%Test plotter
+PlotVtol(u,P)
