b19ab8d11d4fb0c535691dd3d5be20af570706d8
9Volts2Ground <67308127+9Volts2Ground@users.noreply.github.com>
2020-10-28 20:02:11 -0700

    Messed with plotter to add viewing angle. It works... until half way through the sim...

--- 6DoF_Visualizer/drawDrone.m
+++ 6DoF_Visualizer/drawDrone.m
@@ -9,14 +9,30 @@ pitch = u(5);
 yaw = u(6);
 t = u(7);
 
+%Plotting handles
 persistent VTOLbody_handle;
 persistent VTOLrotor1_handle;
 persistent VTOLrotor2_handle;
 persistent VTOLrotor3_handle;
 persistent VTOLrotor4_handle;
+persistent Ground_handle;
+
+%Plotting limits
+persistent x_max
+persistent x_min
+persistent y_max
+persistent y_min
+persistent z_max
+plot_buffer = 1.5;
 
 %First time plotting
 if t==0
+    
+    x_max = x + plot_buffer;
+    x_min = x - plot_buffer;
+    y_max = y + plot_buffer;
+    y_min = y - plot_buffer;
+    z_max = z + plot_buffer;
     
     fprintf('Initializing plot \n')
     figure()
@@ -24,20 +40,49 @@ if t==0
     axis equal
     hold on
     grid on
+    xlabel('X (m)')
+    ylabel('Y (m)')
+    zlabel('Z (m)')
+    view(30, 10)  %Force angled camera view
     %Figure out how to draw a plane to represent the ground
     VTOLbody_handle = drawDroneBody(x,y,z,roll,pitch,yaw,P,[]);
     VTOLrotor1_handle = drawDroneRotor(x,y,z,roll,pitch,yaw,1,P,[]);
     VTOLrotor2_handle = drawDroneRotor(x,y,z,roll,pitch,yaw,2,P,[]);
     VTOLrotor3_handle = drawDroneRotor(x,y,z,roll,pitch,yaw,3,P,[]);
     VTOLrotor4_handle = drawDroneRotor(x,y,z,roll,pitch,yaw,4,P,[]);
-    drawGround(x,y);
+    Ground_handle = drawGround(x,y,[]);
+    xlim([x_min, x_max])
+    ylim([y_min, y_max])
+    zlim([0, z_max])
+    
 %Update plots every other call
 else    
+    
+    fprintf('calculate plot_limits... \n')
+    %Update max plotting limits
+    x_max = max([x_max, x + plot_buffer]);
+    x_min = min([x_min, x - plot_buffer]);
+    y_max = max([y_max, y + plot_buffer]);
+    y_min = min([y_min, y - plot_buffer]);
+    z_max = max([z_max, z + plot_buffer]);
+    
+    fprintf('draw body... \n')
     drawDroneBody(x,y,z,roll,pitch,yaw,P,VTOLbody_handle);    
+    fprintf('draw r1... \n')
     drawDroneRotor(x,y,z,roll,pitch,yaw,1,P,VTOLrotor1_handle);
+    fprintf('draw r2... \n')
     drawDroneRotor(x,y,z,roll,pitch,yaw,2,P,VTOLrotor2_handle);
+    fprintf('draw r3... \n')
     drawDroneRotor(x,y,z,roll,pitch,yaw,3,P,VTOLrotor3_handle);
+    fprintf('draw r4... \n')
     drawDroneRotor(x,y,z,roll,pitch,yaw,4,P,VTOLrotor4_handle);
+    fprintf('draw ground... \n')
+    drawGround(x,y,Ground_handle);
+    
+    fprintf('update lims... \n')
+    xlim([x_min, x_max])
+    ylim([y_min, y_max])
+    zlim([0, z_max])
 end
 
 end
--- 6DoF_Visualizer/drawGround.m
+++ 6DoF_Visualizer/drawGround.m
@@ -1,9 +1,29 @@
-function drawGround(x,y,handle)
+function handle = drawGround(x,y,handle)
 
-ground_size = 5;
+% ground_size = 2;
 
-x_limits = [x-ground_size, x+ground_size];
-y_limits = [y-ground_size, y+ground_size];
+% x_limits = [x-ground_size, x+ground_size];
+% y_limits = [y-ground_size, y+ground_size];
+
+persistent ground_x_max
+persistent ground_x_min
+persistent ground_y_max
+persistent ground_y_min
+
+if isempty(handle)
+    ground_x_max = x + 3;
+    ground_x_min = x - 3;
+    ground_y_max = y + 3;
+    ground_y_min = y - 3;
+else
+    ground_x_max = max([ground_x_max, x+3]);
+    ground_x_min = min([ground_x_min, x-3]);
+    ground_y_max = max([ground_y_max, y+3]);
+    ground_y_min = min([ground_y_min, y-3]);
+end
+
+x_limits = [ground_x_min, ground_x_max];
+y_limits = [ground_y_min, ground_y_max];
 
 [x_ground,y_ground] = meshgrid(x_limits(1):x_limits(2), y_limits(1):y_limits(2));
 
@@ -16,6 +36,10 @@ color = zeros(m, n, 3);
 
 surf(x_ground, y_ground, z_ground, color);
 
-
+if isempty(handle)
+    handle = surf(x_ground, y_ground, z_ground, color);
+else
+    set(handle,'XData', x_ground, 'YData', y_ground, 'ZData', z_ground);
+end
 
 end
